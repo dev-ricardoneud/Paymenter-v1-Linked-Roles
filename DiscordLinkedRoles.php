90facfb6cf3a50ea495d0ae04ca3e51a3760da88
Ricardo Neud <ricardoneud2007@gmail.com>
2025-02-12 21:30:44 +0100

    Update DiscordLinkedRoles.php

--- DiscordLinkedRoles.php
+++ DiscordLinkedRoles.php
@@ -3,14 +3,13 @@
 namespace Paymenter\Extensions\Others\DiscordLinkedRoles;
 
 use App\Classes\Extension\Extension;
-use App\Livewire\Auth\Register;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Event;
-use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\HtmlString;
 
 class DiscordLinkedRoles extends Extension
 {
+    const GITHUB_REPO = 'dev-ricardoneud/Paymenter-v1-Linked-Roles';
+
     public function getConfig($values = [])
     {
         try {
@@ -19,6 +18,11 @@ class DiscordLinkedRoles extends Extension
                     'name' => 'Notice',
                     'type' => 'placeholder',
                     'label' => new HtmlString('Discord Linked Roles Extension, originally created by Corwin, the owner of Paymenter, and later adapted by Ricardo Neud.'),
+                ],
+                [
+                    'name' => 'Version Check',
+                    'type' => 'placeholder',
+                    'label' => new HtmlString($this->getVersion()),
                 ],
             ];
         } catch (\Exception $e) {
@@ -35,5 +39,25 @@ class DiscordLinkedRoles extends Extension
     public function boot()
     {
         require __DIR__ . '/routes/web.php';
+    }
+
+    public function getVersion()
+    {
+        try {
+            $response = Http::get("https://api.github.com/repos/" . self::GITHUB_REPO . "/releases/latest");
+            $latestRelease = $response->json();
+            $latestVersion = $latestRelease['tag_name'] ?? 'unknown';
+            $currentVersion = 'v1.0.1';
+
+            if (version_compare($currentVersion, $latestVersion, '>')) {
+                return 'The version ' . $currentVersion . ' does not exist. Please check the version number.';
+            } elseif ($currentVersion === $latestVersion) {
+                return 'You are using the latest version (' . $latestVersion . ').';
+            } else {
+                return 'You are using version ' . $currentVersion . ', but version ' . $latestVersion . ' is available. Please update!';
+            }
+        } catch (\Exception $e) {
+            return 'Could not check for updates at this time.';
+        }
     }
 }
