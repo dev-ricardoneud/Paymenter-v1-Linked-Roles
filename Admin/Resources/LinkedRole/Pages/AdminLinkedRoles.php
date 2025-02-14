f38001a5d867a12ee5e0ee98098e0ba082937c8e
Ricardo Neud <ricardoneud2007@gmail.com>
2025-02-14 13:11:22 +0100

    Update AdminLinkedRoles.php

--- Admin/Resources/LinkedRole/Pages/AdminLinkedRoles.php
+++ Admin/Resources/LinkedRole/Pages/AdminLinkedRoles.php
@@ -9,4 +9,11 @@ use Paymenter\Extensions\Others\DiscordLinkedRoles\Admin\Resources\LinkedRole;
 class AdminLinkedRoles extends ListRecords
 {
     protected static string $resource = LinkedRole::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
