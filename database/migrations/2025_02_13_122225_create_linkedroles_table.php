6ff3d69f93633f99ad667205221e09bfb3c15dfb
Ricardo Neud <ricardoneud2007@gmail.com>
2025-02-17 15:08:57 +0100

    Update 2025_02_13_122225_create_linkedroles_table.php

--- database/migrations/2025_02_13_122225_create_linkedroles_table.php
+++ database/migrations/2025_02_13_122225_create_linkedroles_table.php
@@ -14,9 +14,20 @@ return new class extends Migration
     {
         Schema::create('linked_role_settings', function (Blueprint $table) {
             $table->id();
+            $table->text('discordlinkedroles_bot_name');
             $table->text('discordlinkedroles_client_id');
             $table->text('discordlinkedroles_client_secret');
             $table->text('discordlinkedroles_bot_token');
+            $table->text('syncedwithpaymenter');
+            $table->text('syncedwithpaymenter_description');
+            $table->text('activeproducts');
+            $table->text('activeproducts_description');
+            $table->text('api_url');
+            $table->text('api_url_version');
+            $table->text('linkedroles_url');
+            $table->text('linkedroles_callback_url');
+            $table->text('callback_redirect_page');
+            $table->text('success_route')->nullable();
         });
     }
 
