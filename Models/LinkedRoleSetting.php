2ba895b57c58237f74421b21ce10641abaeba0ff
Ricardo Neud <ricardoneud2007@gmail.com>
2025-02-17 15:09:47 +0100

    Update LinkedRoleSetting.php

--- Models/LinkedRoleSetting.php
+++ Models/LinkedRoleSetting.php
@@ -3,14 +3,26 @@
 namespace Paymenter\Extensions\Others\DiscordLinkedRoles\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Paymenter\Extensions\Others\DiscordLinkedRoles\Models\LinkedRole;
 
 class LinkedRoleSetting extends Model
 {
     protected $table = 'linked_role_settings';
     protected $fillable = [
+        'discordlinkedroles_bot_name',
         'discordlinkedroles_client_id',
         'discordlinkedroles_client_secret',
         'discordlinkedroles_bot_token',
+        'syncedwithpaymenter',
+        'syncedwithpaymenter_description',
+        'activeproducts',
+        'activeproducts_description',
+        'api_url',
+        'api_url_version',
+        'linkedroles_url',
+        'linkedroles_callback_url',
+        'callback_redirect_page',
+        'success_route',
     ];
     public $timestamps = false;
 
