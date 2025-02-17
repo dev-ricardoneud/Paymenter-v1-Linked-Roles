5bdc256d8c6a8365ddf0fe1c848f2f5f05df3a65
Ricardo Neud <ricardoneud2007@gmail.com>
2025-02-17 15:08:10 +0100

    Update DiscordLinkedRoles.php

--- DiscordLinkedRoles.php
+++ DiscordLinkedRoles.php
@@ -11,18 +11,18 @@ use Paymenter\Extensions\Others\DiscordLinkedRoles\Models\LinkedRoleSetting;
 
 class DiscordLinkedRoles extends Extension
 {
-    const GITHUB_REPO = 'dev-ricardoneud/Paymenter-v1-Linked-Roles';
-    const DISCORD_API_URL = 'https://discord.com/api/v10';
-
     public function getConfig($values = [])
     {
         try {
-            $bots = LinkedRoleSetting::all(['id', 'discordlinkedroles_client_id']);
+            $bots = LinkedRoleSetting::all(['id', 'discordlinkedroles_bot_name']);
             $botOptions = $bots->filter(function ($bot) {
-                return !empty($bot->discordlinkedroles_client_id);
-            })->pluck('discordlinkedroles_client_id', 'id')->toArray();
+                return !empty($bot->discordlinkedroles_bot_name);
+            })->pluck('discordlinkedroles_bot_name', 'id')->toArray();
             
             $selectedBotId = Setting::where('key', 'discord_bot_id')->value('value');
+            $discordBot = LinkedRoleSetting::where('id', $selectedBotId)->first();
+            $discordBotName = $discordBot ? $discordBot->discordlinkedroles_bot_name : 'No bot';
+            
             return [
                 [
                     'name' => 'Notice',
@@ -39,8 +39,10 @@ class DiscordLinkedRoles extends Extension
                     'type' => 'select',
                     'label' => 'Select a Discord Bot',
                     'options' => $botOptions,
-                    'description' => new HtmlString($this->getDiscordBotName($selectedBotId)),
+                    'description' => 'Linked Roles connected with ' . $discordBotName,
                     'value' => $selectedBotId,
+                    'disabled' => false,
+                    'live' => true,
                 ],
                 [
                     'name' => 'Discord Bot Connections',
@@ -49,11 +51,31 @@ class DiscordLinkedRoles extends Extension
                 ],
             ];
         } catch (\Exception $e) {
+            $selectedBotId = Setting::where('key', 'discord_bot_id')->value('value');
+            $discordBot = LinkedRoleSetting::where('id', $selectedBotId)->first();
+            $discordBotName = $discordBot ? $discordBot->discordlinkedroles_bot_name : 'No bot selected';
             return [
                 [
                     'name' => 'Notice',
                     'type' => 'placeholder',
                     'label' => new HtmlString('Discord Linked Roles Extension, originally created by Corwin, the owner of Paymenter, and later adapted by Ricardo Neud.'),
+                ],
+                [
+                    'name' => 'Version Check',
+                    'type' => 'placeholder',
+                    'label' => new HtmlString($this->getVersion()),
+                ],
+                [
+                    'name' => 'discord_bot_id',
+                    'type' => 'select',
+                    'label' => 'Select a Discord Bot',
+                    'options' => [
+                        '' . $selectedBotId . '' => 'No Bots Found',
+                    ],
+                    'description' => 'Linked Roles connected with ' . $discordBotName,
+                    'value' => $selectedBotId,
+                    'disabled' => true,
+                    'live' => true,
                 ],
             ];
         }
@@ -61,7 +83,10 @@ class DiscordLinkedRoles extends Extension
 
     public function enabled()
     {
-        Artisan::call('migrate', ['--path' => 'extensions/Others/DiscordLinkedRoles/database/migrations/2025_02_13_122225_create_linkedroles_table.php']);
+        Artisan::call('migrate', ['--path' => [
+            'extensions/Others/DiscordLinkedRoles/database/migrations/2025_02_13_122225_create_linkedroles_table.php',
+            'extensions/Others/DiscordLinkedRoles/database/migrations/2025_02_13_122225_create_linkedroles_custom_pages_table.php'
+        ]]);
     }
 
     public function boot()
@@ -72,13 +97,13 @@ class DiscordLinkedRoles extends Extension
     public function getVersion()
     {
         try {
-            $response = Http::get("https://api.github.com/repos/" . self::GITHUB_REPO . "/releases/latest");
+            $response = Http::get("https://api.github.com/repos/dev-ricardoneud/Paymenter-v1-Linked-Roles/releases/latest");
             $latestRelease = $response->json();
             if (!is_array($latestRelease) || !isset($latestRelease['tag_name'])) {
                 return 'Could not check for updates at this time.';
             }
             $latestVersion = $latestRelease['tag_name'];
-            $currentVersion = 'v1.0.4';
+            $currentVersion = 'v1.0.5';
             if (version_compare($currentVersion, $latestVersion, '>')) {
                 return 'The version ' . $currentVersion . ' does not exist. If this is the main branch, it may contain errors. Please downgrade to the latest stable version (' . $latestVersion . ') to avoid potential issues.';
             } elseif ($currentVersion === $latestVersion) {
@@ -103,7 +128,7 @@ class DiscordLinkedRoles extends Extension
             }
             $response = Http::withHeaders([
                 'Authorization' => 'Bot ' . $bot->discordlinkedroles_bot_token,
-            ])->get(self::DISCORD_API_URL . '/applications/@me');
+            ])->get('https://' . $bot->api_url . '/api/' . $bot->api_url_version . '/applications/@me');
             if ($response->failed()) {
                 return 'Could not retrieve bot connection data at this time.';
             }
@@ -112,32 +137,6 @@ class DiscordLinkedRoles extends Extension
                 return 'Could not retrieve bot connection data at this time.';
             }
             return 'The bot ' . $data['name'] . ' is authorized by approximately ' . $data['approximate_user_install_count'] . ' users.';
-        } catch (\Exception $e) {
-            return 'Could not retrieve bot connection data at this time.';
-        }
-    }
-
-    public function getDiscordBotName($botId = null)
-    {
-        try {
-            if (!$botId) {
-                return 'No bot selected.';
-            }
-            $bot = LinkedRoleSetting::where('id', $botId)->first();
-            if (!$bot) {
-                return 'Invalid bot selection.';
-            }
-            $response = Http::withHeaders([
-                'Authorization' => 'Bot ' . $bot->discordlinkedroles_bot_token,
-            ])->get(self::DISCORD_API_URL . '/applications/@me');
-            if ($response->failed()) {
-                return 'Could not retrieve bot name at this time.';
-            }
-            $data = $response->json();
-            if (!is_array($data) || !isset($data['name'])) {
-                return 'Could not retrieve bot name at this time.';
-            }
-            return 'Linked Roles connected with ' . $data['name'] . '';
         } catch (\Exception $e) {
             return 'Could not retrieve bot connection data at this time.';
         }
